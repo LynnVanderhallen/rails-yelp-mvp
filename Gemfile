2f11dbd70db371bbe522db21b29c1aeb7a90fb45
Lynn Vanderhallen <lynn.vanderhallen@gmail.com>
2017-11-01 17:05:55 +0100

    Finished challenge 1

--- Gemfile
+++ Gemfile
@@ -5,7 +5,11 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.6'
 # Use sqlite3 as the database for Active Record
@@ -21,8 +25,6 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
-# Use jquery as the JavaScript library
-gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: "Epicure au Bristol",
+    address: "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "+33 1 53 43 43 40",
+    category: "french",
+  },
+  {
+    name: "La truffière",
+    address: "4 rue Blainville 75005 Paris",
+    phone_number: "+33 1 46 33 29 82",
+    category: "french",
+  },
+  {
+    name: "Le pré catelan",
+    address: "route de Suresnes 75016 Paris",
+    phone_number: "+33 1 44 14 41 14",
+    category: "french",
+  },
+  {
+    name: "The Jane",
+    address: "paradeplein 1, 2018 Antwerpen",
+    phone_number: "+32 3 808 44 65",
+    category: "belgian",
+  },
+  {
+    name: "San Daniele",
+    address: "avenue Charles-Quint 6, 1083 Bruxelles",
+    phone_number: "+32 2 426 79 23",
+    category: "italian",
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
